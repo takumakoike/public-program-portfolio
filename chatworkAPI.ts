76e0ae5267d81604ab4fb408fb1371f6a1e89077
takumakoike <takuma.151e@gmail.com>
2025-01-24 19:58:33 +0900

    getするfetchデータをリファクタリング

--- chatworkAPI.ts
+++ chatworkAPI.ts
@@ -1,6 +1,19 @@
 const chatwork_API = process.env.chatworkAPI__CW_API;
 const chatwork_URL = "https://api.chatwork.com/v2/";
 
+function getFetchData(chatwork_API: string, url: string): JSON{
+  const getOptions: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    headers:  {
+      "X-ChatWorkToken": chatwork_API,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    method: "get"
+  }
+  const fetchData: GoogleAppsScript.URL_Fetch.HTTPResponse = UrlFetchApp.fetch(url,getOptions);
+  
+  return JSON.parse(fetchData.getContentText());
+}
+
 //自分の情報を取得するスクリプト
 function getMyChatWorkAccoutData(): {account_id: string, chatwork_id: string, name: string, mail: string} | Error{
   const url = `${chatwork_URL}/me`;
@@ -28,17 +41,7 @@ function getMyRoomInfo(): {roomId: string, roomName: string}[] | Error{
   const url = `${chatwork_URL}/rooms`;
   if(!chatwork_API) return new Error("チャットワークAPIキーが見つかりませんでした。")
 
-  const getOptions: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
-    headers:  {
-      "X-ChatWorkToken": chatwork_API,
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    method: "get"
-  }
-
-  const fetchData: GoogleAppsScript.URL_Fetch.HTTPResponse = UrlFetchApp.fetch(url,getOptions);
-  const jsonData = JSON.parse(fetchData.getContentText());
-
+  const jsonData = "";
   const roomInfo: {roomId: string, roomName: string}[] = [];
   for (const room of jsonData){
     roomInfo.push({roomId: room.room_id, roomName: room.name});
@@ -46,6 +49,45 @@ function getMyRoomInfo(): {roomId: string, roomName: string}[] | Error{
 
   return roomInfo
 }
+
+
+//自分の現状ステータスを取得する→OK
+function myTaskInformation() {
+  const url =`${chatwork_URL}/my/status`;
+  if(!chatwork_API) return new Error("チャットワークAPIキーが見つかりませんでした。")
+  const getOptions: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    headers:  {
+      "X-ChatWorkToken": chatwork_API,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    method: "get"
+  }
+  const fetchData = UrlFetchApp.fetch(url,getOptions);
+
+    const taskData = JSON.parse(myTasks);
+    console.log(taskData["mytask_num"]);
+    console.log(taskData.mytask_num);
+}
+
+ //自分のタスクを取得する
+ function MYTASK(){
+   const url_base = "https://api.chatwork.com/v2/my/tasks?";
+     //assigned_by_account_id=78& /*タスクを依頼した人を特定したい場合にはこれを入力してfetchにかける*/
+   const url_assigned = "assigned_by_account_id=" + '**********' +"&"; //アスタリスク部分『**********』を変更する
+   const url_status = "status=done";
+ 
+   const url = url_base + url_status;
+ //  console.log(url);
+ 
+     const myT = UrlFetchApp.fetch(url,param);
+     const tasukude_ta = JSON.parse(myT);
+     for(const i=0; i<=10; i++){
+         console.log(tasukude_ta[i].limit_time);
+       };
+ }
+ //ここまで自分のタスクを取得する
+ 
+ 
 
 
 
@@ -105,39 +147,6 @@ function getGroupMembers(){
 }
 
 
-
-
-//自分の現状ステータスを取得する→OK
-function myFunction() {
- const cw_url ="https://api.chatwork.com/v2/my/status";
-// console.log(cw_url);
-    const myTasks = UrlFetchApp.fetch(cw_url,{
-    headers : header,
-    method: "GET"
-    });
-    const taskData = JSON.parse(myTasks);
-    console.log(taskData["mytask_num"]);
-    console.log(taskData.mytask_num);
-}
-/*ここまで自分の現状ステータスを取得する関数*/
-
-//自分のタスクを取得する
-function MYTASK(){
-  const url_base = "https://api.chatwork.com/v2/my/tasks?";
-    //assigned_by_account_id=78& /*タスクを依頼した人を特定したい場合にはこれを入力してfetchにかける*/
-  const url_assigned = "assigned_by_account_id=" + '**********' +"&"; //アスタリスク部分『**********』を変更する
-  const url_status = "status=done";
-
-  const url = url_base + url_status;
-//  console.log(url);
-
-    const myT = UrlFetchApp.fetch(url,param);
-    const tasukude_ta = JSON.parse(myT);
-    for(const i=0; i<=10; i++){
-        console.log(tasukude_ta[i].limit_time);
-      };
-}
-//ここまで自分のタスクを取得する
 
 
 //チャット情報を取得するスクリプト
